058fece44b5988e045d3cc8999cd95d5ee52edc8
barrytra <BHAVESHGUPTA1602@GMAIL.COM>
2024-04-23 00:26:08 +0530

    fixed message serialisation

--- utils/serializeBlockHeader.js
+++ utils/serializeBlockHeader.js
@@ -16,7 +16,7 @@ function serializeBlockHeader(version, prevBlockHash, merkleRoot, timestamp, bit
     buffer = Buffer.concat([buffer, Buffer.from((prevBlockHash), "hex").reverse()]);
 
     // merkleRoot
-    buffer = Buffer.concat([buffer, Buffer.from((merkleRoot), "hex").reverse()]);
+    buffer = Buffer.concat([buffer, Buffer.from((merkleRoot), "hex")]);
 
     // timestamp
     buffer = Buffer.concat([buffer, Buffer.from(timestamp.toString(16).padStart(8, '0'), 'hex').reverse()]);
